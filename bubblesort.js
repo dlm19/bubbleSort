8aa46202cc639b72a693d05d8278c1df4b82a826
Dani <dani.meyer.nd@gmail.com>
2020-02-18 12:48:56 -0500

    passing all specs

--- bubblesort.js
+++ bubblesort.js
@@ -1,7 +1,30 @@
 function swap(input1, input2) {
-    
+  if (input1 === input2) {
+    return 'you should not have two of the same number';
+  }
+
+  if (input1 > input2) {
+    return [input2, input1];
+  } else {
+    return [input1, input2];
+  }
 }
 
-function bubbleSort(array) {
+function bubbleSort(array, finalArr = []) {
+  if (array.length <= 1) {
+    return array;
+  } else if (array.length === 2) {
+    finalArr.unshift(...array);
+  } else {
+    for (let i = 0; i < array.length - 1; i++) {
+      let swappedVals = swap(array[i], array[i + 1]);
+      array.splice(i, 2, ...swappedVals);
+    }
+    let val = array.pop();
+    finalArr.unshift(val);
+    console.log(finalArr);
+    bubbleSort(array, finalArr);
+  }
 
+  return finalArr;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,32 +1,42 @@
 describe('Swap', function() {
-    it('takes two number inputs and returns them in numerical order', function() {
-        expect(swap(8,2)).toEqual(2,8)
-        expect(swap(2,9)).toEqual(2,9)
-        expect(swap(3,3)).toEqual('you should not have two of the same number')
-    })
-})
+  it('takes two number inputs and returns them in numerical order', function() {
+    expect(swap(8, 2)).toEqual([2, 8]);
+    expect(swap(2, 9)).toEqual([2, 9]);
+    expect(swap(3, 3)).toEqual('you should not have two of the same number');
+  });
+});
 
 describe('Bubble Sort', function() {
-    beforeAll(function() {
-        spyOn(window, 'swap').and.callThrough();
-    })
-   it('handles an empty array', function() {
-      expect(bubbleSort([])).toEqual([])
-      ;
-      expect(window.swap.calls.count()).toEqual(0)
-   });
-   it('handles a single item in an array', function() {
-       expect(bubbleSort([4])).toEqual([4])
-       expect(window.swap.calls.count()).toEqual(0);
-   });
-   it('handles an array with multiple items', function() {
-       expect(bubbleSort([5,8,2,9,6])).toEqual([2,5,6,8,9])
-       expect(window.swap.calls.count()).toEqual(9);
+  beforeEach(function() {
+    spyOn(window, 'swap').and.callThrough();
+  });
+  it('handles an empty array', function() {
+    expect(bubbleSort([])).toEqual([]);
+    expect(window.swap.calls.count()).toEqual(0);
+  });
+  it('handles a single item in an array', function() {
+    expect(bubbleSort([4])).toEqual([4]);
+    expect(window.swap.calls.count()).toEqual(0);
+  });
+  it('handles an array with multiple items', function() {
+    expect(bubbleSort([5, 8, 2, 9, 6])).toEqual([2, 5, 6, 8, 9]);
+    expect(window.swap.calls.count()).toEqual(9);
     // or might be 10
-   })
-   it('handles a longer array with multiple items', function() {
-       expect(bubbleSort([33, 99, 50, 1, 15, 72, 98, 45, 4, 68]))
-       expect(window.swap.calls.count()).toEqual(44);
+  });
+  it('handles a longer array with multiple items', function() {
+    expect(bubbleSort([33, 99, 50, 1, 15, 72, 98, 45, 4, 68])).toEqual([
+      1,
+      4,
+      15,
+      33,
+      45,
+      50,
+      68,
+      72,
+      98,
+      99,
+    ]);
+    expect(window.swap.calls.count()).toEqual(44);
     // or might be 45
-   })
+  });
 });
